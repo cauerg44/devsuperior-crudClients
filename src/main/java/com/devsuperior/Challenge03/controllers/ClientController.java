161bbeca9283f2bbc4d0218d7b578ff809a93c3c
Caue Garcia <cauerg7@outlook.com>
2023-11-23 15:11:29 -0300

    insert implementation POST

--- src/main/java/com/devsuperior/Challenge03/controllers/ClientController.java
+++ src/main/java/com/devsuperior/Challenge03/controllers/ClientController.java
@@ -1,18 +1,25 @@
 package com.devsuperior.challenge03.controllers;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.challenge03.dto.ClientDTO;
 import com.devsuperior.challenge03.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
-public class ClientControllers {
+public class ClientController {
 
 	@Autowired
 	private ClientService service;
@@ -21,5 +28,18 @@ public class ClientControllers {
 	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
 		ClientDTO dto = service.findById(id);
 		return ResponseEntity.ok(dto);
+	}
+	
+	@GetMapping
+	public List<ClientDTO> findAll(Pageable pageable){
+		return service.findAll(pageable);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
--- src/main/java/com/devsuperior/Challenge03/services/ClientService.java
+++ src/main/java/com/devsuperior/Challenge03/services/ClientService.java
@@ -1,6 +1,10 @@
 package com.devsuperior.challenge03.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,5 +22,27 @@ public class ClientService {
 	public ClientDTO findById(Long id) {
 		Client client = repository.findById(id).get();
 		return new ClientDTO(client);
+	}
+	
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findAll(Pageable pageable) {
+		Page<Client> search = repository.findAll(pageable);
+		return search.stream().map(x -> new ClientDTO(x)).toList();
+	}
+	
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client client = new Client();
+		dtoToEntity(dto, client);
+		client = repository.save(client);
+		return new ClientDTO(client);
+	}
+
+	private void dtoToEntity(ClientDTO dto, Client client) {
+		client.setName(dto.getName());
+		client.setCpf(dto.getCpf());
+		client.setIncome(dto.getIncome());
+		client.setBirthDate(dto.getBirthDate());
+		client.setChildren(dto.getChildren());
 	}
 }
